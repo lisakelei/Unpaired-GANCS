7cd242421058f1fe6f693145b3ebb3b3a7371e4f
lisakelei <lisalei67@gmail.com>
2018-11-04 00:35:24 -0700

    Update wgancs_input.py
    clean ups

--- wgancs_input.py
+++ wgancs_input.py
@@ -1,49 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import math as math
-
 FLAGS = tf.app.flags.FLAGS
-
-# generate mask based on alpha
-def generate_mask_alpha(size=[128,128], r_factor_designed=5.0, r_alpha=3, axis_undersample=1,
-                        acs=3, seed=0, mute=0):
-    # init
-    mask = np.zeros(size)
-    if seed>=0:
-        np.random.seed(seed)
-    # get samples
-    num_phase_encode = size[axis_undersample]
-    num_phase_sampled = int(np.floor(num_phase_encode/r_factor_designed))
-    # coordinate
-    coordinate_normalized = np.array(range(num_phase_encode))
-    coordinate_normalized = np.abs(coordinate_normalized-num_phase_encode/2)/(num_phase_encode/2.0)
-    prob_sample = coordinate_normalized**r_alpha
-    prob_sample = prob_sample/sum(prob_sample)
-    # sample
-    index_sample = np.random.choice(num_phase_encode, size=num_phase_sampled, 
-                                    replace=False, p=prob_sample)
-    # sample                
-    if axis_undersample == 0:
-        mask[index_sample,:]=1
-    else:
-        mask[:,index_sample]=1
-
-    # acs                
-    if axis_undersample == 0:
-        mask[:int((acs+1)/2),:]=1
-        mask[-int(acs/2):,:]=1
-    else:
-        mask[:,:int((acs+1)/2)]=1
-        mask[:,-int(acs/2):]=1
-
-    # compute reduction
-    r_factor = len(mask.flatten())/sum(mask.flatten())
-    if not mute:
-        print('gen mask size of {1} for R-factor={0:.4f}'.format(r_factor, mask.shape))
-        #print(num_phase_encode, num_phase_sampled, np.where(mask[0,:]))
-
-    return mask, r_factor
-
 
 # generate mask based on .mat mask
 def generate_mask_mat(mask=[], mute=0):
@@ -55,7 +13,6 @@ def generate_mask_mat(mask=[], mute=0):
         print('load mask size of {1} for R-factor={0:.4f}'.format(r_factor, mask.shape))
     return mask, r_factor
 
-
 def setup_inputs_one_sources(sess, filenames_input, filenames_output, image_size=None, label_size=None, 
                              axis_undersample=1, capacity_factor=1, 
                              r_factor=4, r_alpha=0, r_seed=0,
@@ -63,7 +20,6 @@ def setup_inputs_one_sources(sess, filenames_input, filenames_output, image_size
 
     # image size
     if image_size is None:
-        # image_size
         if FLAGS.sample_size_y>0:
             image_size = [FLAGS.sample_size, FLAGS.sample_size_y]
         else:
@@ -71,12 +27,7 @@ def setup_inputs_one_sources(sess, filenames_input, filenames_output, image_size
 
     # generate default mask
     if sampling_mask is None:
-        DEFAULT_MASK, _ = generate_mask_alpha(image_size, # kspace size
-                                              r_factor_designed=r_factor, 
-                                              r_alpha=r_alpha, 
-                                              seed=r_seed,
-                                              axis_undersample=axis_undersample
-                                              )
+        print('nomask!!')
     else:
         # get input mask
         DEFAULT_MASK, _ = generate_mask_mat(sampling_mask)
@@ -85,7 +36,7 @@ def setup_inputs_one_sources(sess, filenames_input, filenames_output, image_size
     DEFAULT_MAKS_TF = tf.cast(tf.constant(DEFAULT_MASK), tf.float32)
     DEFAULT_MAKS_TF_c = tf.cast(DEFAULT_MAKS_TF, tf.complex64)
 
-    # Read each JPEG file
+    # Read each np file
     reader_input = tf.WholeFileReader()
     filename_queue_input = tf.train.string_input_producer(filenames_input, shuffle=False)
     _key, value_input = reader_input.read(filename_queue_input)
@@ -123,16 +74,6 @@ def setup_inputs_one_sources(sess, filenames_input, filenames_output, image_size
 
     print('image_input_complex size', image_input.get_shape())
     print('image_output_complex size', image_output.get_shape())
-
-
-    ##choose the magnitude
-    #image_input = image_input[0:image_size[0],0:image_size[1]]
-
-    # cast image to float in 0~1
-    #image_input = tf.cast(image_input, tf.float32)/255.0
-
-    # output, gold-standard
-    #image_output = image_input
 
     # apply undersampling mask
     kspace_input = tf.fft2d(tf.cast(image_input,tf.complex64))
